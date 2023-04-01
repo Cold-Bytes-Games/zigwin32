4fa215632ffbe33b5008c1fe7532eeb70b6fc0ad
mlarouche <michael.larouche@gmail.com>
2023-04-01 13:52:17 -0400

    Add target and optimize

--- build.zig
+++ build.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    _ = b.standardTargetOptions(.{});
+    _ = b.standardOptimizeOption(.{});
+
     _ = b.addModule("zigwin32", .{
         .source_file = .{ .path = "win32.zig" },
     });
